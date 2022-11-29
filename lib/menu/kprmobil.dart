2e4cb3bc55a8e1890a862649706ba889fac45402
geraldineDafa <dafageraldine16@gmail.com>
2022-11-29 20:58:01 +0700

    kpr mobil

---
+++ lib/menu/kprmobil.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class KprMobil extends StatefulWidget {
+  const KprMobil({super.key});
+
+  @override
+  State<KprMobil> createState() => _KprMobilState();
+}
+
+class _KprMobilState extends State<KprMobil> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(),
+    );
+  }
+}
