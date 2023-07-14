48d574c5a38117beddcb36547e0de35c2e040dc5
geraldineDafa <43980690+dafageraldine@users.noreply.github.com>
2023-07-14 15:43:36 +0700

    progress calculate target

--- lib/screens/submenu/calculatetarget.dart
+++ lib/screens/submenu/calculatetarget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:recordinvest/components/app_bar_with_back_button.dart';
+import 'package:recordinvest/components/processbutton.dart';
 
 class CalculateTarget extends StatelessWidget {
   // const CalculateTarget({super.key});
@@ -17,7 +19,35 @@ class CalculateTarget extends StatelessWidget {
             onTap: () {
               Get.back();
             },
-          )
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 0.02.sh, left: 0.1.sw),
+            child: const Text(
+              "Enter Target",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Color.fromRGBO(157, 157, 157, 1),
+                fontSize: 16,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 0.8.sw,
+            height: 0.07.sh,
+            child: TextFormField(
+                // controller:
+                // _stockAnalysisController.values.value,
+                // obscureText: true,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Color.fromRGBO(157, 157, 157, 0.5),
+                      fontSize: 16,
+                    ),
+                    hintText: "15000000.00")),
+          ),
+          ProcessButton(title: "calculate")
         ],
       )),
     );
