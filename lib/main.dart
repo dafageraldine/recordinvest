8ea757613d1838dc732df4a8c837469f516a408f
geraldineDafa <43980690+dafageraldine@users.noreply.github.com>
2023-07-25 14:09:03 +0700

    added one signal

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:recordinvest/models/data.dart';
 import 'package:recordinvest/screens/splash/splash.dart';
 
 Future<void> main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
+    //Remove this method to stop OneSignal Debugging
+    OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+    OneSignal.shared.setAppId("9ccee615-aa11-4644-abe8-781c0186ebb8");
+
+    // The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+    OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+      print("Accepted permission: $accepted");
+    });
     camerasg = await availableCameras();
   } on CameraException catch (e) {
     Get.snackbar("error", e.toString());
