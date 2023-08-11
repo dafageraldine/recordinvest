f5cc6a4827acea9fda0c70db492baaf0753110e3
geraldineDafa <43980690+dafageraldine@users.noreply.github.com>
2023-08-11 09:05:44 +0700

    create widget list setting

---
+++ lib/components/listsetting.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:recordinvest/models/data.dart';
+
+class ListSetting extends StatelessWidget {
+  var ontap;
+  String title;
+  String desc;
+  IconData icon;
+  ListSetting(
+      {super.key,
+      required ontap,
+      required this.title,
+      required this.desc,
+      required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 0.1.sw),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title,
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w600, fontSize: 14.sp)),
+                Text(desc,
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.normal, fontSize: 12.sp))
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 0.1.sw),
+              child: InkWell(
+                onTap: ontap,
+                child: Container(
+                  width: 0.15.sw,
+                  height: 0.05.sh,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: theme,
+                  ),
+                  child: Icon(icon, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:recordinvest/components/app_bar_only.dart';
 import 'package:recordinvest/components/appversion.dart';
+import 'package:recordinvest/components/listsetting.dart';
 import 'package:recordinvest/controller/settingcontroller.dart';
 import 'package:recordinvest/models/data.dart';
 import 'package:recordinvest/screens/camera/camera.dart';
@@ -31,45 +32,30 @@ class SettingsPage extends StatelessWidget {
           indent: 30,
           endIndent: 30,
         ),
-        Padding(
-          padding: EdgeInsets.only(left: 0.1.sw),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Log Out",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600, fontSize: 14.sp)),
-                    Text("Exit Application",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.normal, fontSize: 12.sp))
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 0.1.sw),
-                  child: InkWell(
-                    onTap: () {
-                      _settingController.showAlertDialog();
-                    },
-                    child: Container(
-                      width: 0.15.sw,
-                      height: 0.05.sh,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: theme,
-                      ),
-                      child: const Icon(Icons.exit_to_app_rounded,
-                          color: Colors.white),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+        ListSetting(
+            ontap: () {
+              _settingController.showAlertDialog();
+            },
+            title: "Log Out",
+            icon: Icons.exit_to_app_rounded,
+            desc: "Exit Application"),
+        5.verticalSpace,
+        Divider(
+          color: Colors.grey.shade300,
+          thickness: 2,
+          indent: 30,
+          endIndent: 30,
+        ),
+        ListSetting(
+          ontap: () {
+            Get.to(InitialPage(
+              urlweb: 'http://dafageraldine.pythonanywhere.com/',
+              title: "Developer",
+            ));
+          },
+          title: "Developer Profile",
+          desc: "About Developer Profile",
+          icon: Icons.code_rounded,
         ),
         5.verticalSpace,
         Divider(
@@ -78,48 +64,13 @@ class SettingsPage extends StatelessWidget {
           indent: 30,
           endIndent: 30,
         ),
-        Padding(
-          padding: EdgeInsets.only(left: 0.1.sw),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Developer Profile",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600, fontSize: 14.sp)),
-                    Text("About Developer Profile",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.normal, fontSize: 12.sp))
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 0.1.sw),
-                  child: InkWell(
-                    onTap: () {
-                      Get.to(InitialPage(
-                        urlweb: 'http://dafageraldine.pythonanywhere.com/',
-                        title: "Developer",
-                      ));
-                    },
-                    child: Container(
-                      width: 0.15.sw,
-                      height: 0.05.sh,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: theme,
-                      ),
-                      child:
-                          const Icon(Icons.code_rounded, color: Colors.white),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+        ListSetting(
+          ontap: () {
+            Get.to(UpdateStock());
+          },
+          title: "Update Stock",
+          desc: "Update historical price data",
+          icon: Icons.web_sharp,
         ),
         5.verticalSpace,
         Divider(
@@ -128,44 +79,16 @@ class SettingsPage extends StatelessWidget {
           indent: 30,
           endIndent: 30,
         ),
-        Padding(
-          padding: EdgeInsets.only(left: 0.1.sw),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Update Stock",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600, fontSize: 14.sp)),
-                    Text("Update historical price data",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.normal, fontSize: 12.sp))
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 0.1.sw),
-                  child: InkWell(
-                    onTap: () {
-                      Get.to(UpdateStock());
-                    },
-                    child: Container(
-                      width: 0.15.sw,
-                      height: 0.05.sh,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: theme,
-                      ),
-                      child: const Icon(Icons.web_sharp, color: Colors.white),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+        ListSetting(
+          ontap: () {
+            Get.to(InitialPage(
+              urlweb: 'https://www.pythonanywhere.com/',
+              title: 'Webserver',
+            ));
+          },
+          title: "Extend Server",
+          desc: "Extend Webserver",
+          icon: Icons.computer_rounded,
         ),
         5.verticalSpace,
         Divider(
@@ -174,98 +97,16 @@ class SettingsPage extends StatelessWidget {
           indent: 30,
           endIndent: 30,
         ),
-        Padding(
-          padding: EdgeInsets.only(left: 0.1.sw),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Extend Server",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600, fontSize: 14.sp)),
-                    Text("Extend Webserver",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.normal, fontSize: 12.sp))
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 0.1.sw),
-                  child: InkWell(
-                    onTap: () {
-                      Get.to(InitialPage(
-                        urlweb: 'https://www.pythonanywhere.com/',
-                        title: 'Webserver',
-                      ));
-                    },
-                    child: Container(
-                      width: 0.15.sw,
-                      height: 0.05.sh,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: theme,
-                      ),
-                      child: const Icon(Icons.computer_rounded,
-                          color: Colors.white),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-        5.verticalSpace,
-        Divider(
-          color: Colors.grey.shade300,
-          thickness: 2,
-          indent: 30,
-          endIndent: 30,
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 0.1.sw),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Other Menu",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600, fontSize: 14.sp)),
-                    Text("Access Webview Menu",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.normal, fontSize: 12.sp))
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 0.1.sw),
-                  child: InkWell(
-                    onTap: () {
-                      Get.to(InitialPage(
-                        urlweb:
-                            'https://dafageraldine.pythonanywhere.com/webview',
-                        title: 'Other Menu',
-                      ));
-                    },
-                    child: Container(
-                      width: 0.15.sw,
-                      height: 0.05.sh,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: theme,
-                      ),
-                      child: const Icon(Icons.link_sharp, color: Colors.white),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+        ListSetting(
+          ontap: () {
+            Get.to(InitialPage(
+              urlweb: 'https://dafageraldine.pythonanywhere.com/webview',
+              title: 'Other Menu',
+            ));
+          },
+          title: "Other Menu",
+          desc: "Access Webview Menu",
+          icon: Icons.link_sharp,
         ),
         5.verticalSpace,
         Divider(
